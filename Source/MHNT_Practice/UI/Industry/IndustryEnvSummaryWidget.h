00f575713ee1440ca1fcf1511fc4f284bafef896
kykim9050 <kykim9050@naver.com>
2025-01-22 14:13:42 +0900

    김경윤 : EnvSummaryWidget 구현

---
+++ Source/MHNT_Practice/UI/Industry/IndustryEnvSummaryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Industry/Base/BaseIndustryWidget.h"
+#include "IndustryEnvSummaryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHNT_PRACTICE_API UIndustryEnvSummaryWidget : public UBaseIndustryWidget
+{
+	GENERATED_BODY()
+	
+};
