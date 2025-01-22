f0205b6b1972421eb670c835bf567eb79e91fec2
kykim9050 <kykim9050@naver.com>
2025-01-22 11:00:58 +0900

    김경윤 : BaseIndustryWidget, SummaryIndustryWidget 추가

---
+++ Source/MHNT_Practice/UI/Industry/SummaryIndustryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Industry/Base/BaseIndustryWidget.h"
+#include "SummaryIndustryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHNT_PRACTICE_API USummaryIndustryWidget : public UBaseIndustryWidget
+{
+	GENERATED_BODY()
+	
+};
