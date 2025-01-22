c1e3caadd116b82a455d99bddcb2910a54eb9863
kykim9050 <kykim9050@naver.com>
2025-01-22 10:02:15 +0900

    김경윤 : BaseUserWidget, IndustryMainWidget 추가

---
+++ Source/MHNT_Practice/UI/Industry/IndustryMainWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/BaseUserWidget.h"
+#include "IndustryMainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHNT_PRACTICE_API UIndustryMainWidget : public UBaseUserWidget
+{
+	GENERATED_BODY()
+	
+};
