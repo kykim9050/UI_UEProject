691273b442eae2dc046ccd99d8373ac29544f80d
kykim9050 <kykim9050@naver.com>
2025-01-23 12:41:33 +0900

    김경윤 : IndustryTBMChart 차트 라인 구현

--- Source/MHNT_Practice/UI/Industry/IndustryTBMChartWidget.h
+++ Source/MHNT_Practice/UI/Industry/IndustryTBMChartWidget.h
@@ -13,5 +13,31 @@ UCLASS()
 class MHNT_PRACTICE_API UIndustryTBMChartWidget : public UBaseIndustryWidget
 {
 	GENERATED_BODY()
-	
+public:
+
+    //UFUNCTION(BlueprintCallable)
+	void SetGraphData(const TArray<FVector2D>& InDataPoints);
+
+protected:
+    // Override OnPaint to draw the graph
+    virtual int32 NativePaint(
+        const FPaintArgs& Args,
+        const FGeometry& AllottedGeometry,
+        const FSlateRect& MyCullingRect,
+        FSlateWindowElementList& OutDrawElements,
+        int32 LayerId,
+        const FWidgetStyle& InWidgetStyle,
+        bool bParentEnabled
+    ) const override;
+
+private:
+
+
+protected:
+
+private:
+	// Data points for the graph
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Graph", meta = (AllowprivateAccess = "true"))
+	TArray<FVector2D> DataPoints;
+
 };
