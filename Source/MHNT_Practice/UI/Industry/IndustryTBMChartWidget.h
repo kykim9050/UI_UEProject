4ee82ff9eefc88df4eda9eb986856f430599e077
kykim9050 <kykim9050@naver.com>
2025-01-23 10:26:16 +0900

    김경윤 : IndustryTBMChartWidget 추가

---
+++ Source/MHNT_Practice/UI/Industry/IndustryTBMChartWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Industry/Base/BaseIndustryWidget.h"
+#include "IndustryTBMChartWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHNT_PRACTICE_API UIndustryTBMChartWidget : public UBaseIndustryWidget
+{
+	GENERATED_BODY()
+	
+};
