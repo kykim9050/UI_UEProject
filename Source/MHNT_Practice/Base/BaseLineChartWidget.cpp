eb26ecc8ce9f78a86d0d3216fb856a70b305e091
kykim9050 <kykim9050@naver.com>
2025-01-23 13:56:30 +0900

    김경윤 : 그래프 점 찍기

--- Source/MHNT_Practice/Base/BaseLineChartWidget.cpp
+++ Source/MHNT_Practice/Base/BaseLineChartWidget.cpp
@@ -50,5 +50,29 @@ int32 UBaseLineChartWidget::NativePaint(
         }
     }
 
+    // 데이터 포인트 그리기
+    if (DataPoints.Num() > 0)
+    {
+        for (const FVector2D& Point : DataPoints)
+        {
+            FVector2D DrawPosition = Point * CanvasSize;
+
+            // 원의 크기와 색상 설정
+            FVector2D CircleSize(8.0f, 8.0f); // 반지름 4.0f
+            FSlateBrush CircleBrush;
+            CircleBrush.TintColor = FLinearColor::Green; // 원의 색상
+
+            // 원 그리기
+            FSlateDrawElement::MakeBox(
+                OutDrawElements,
+                CurrentLayer,
+                AllottedGeometry.ToPaintGeometry(DrawPosition - CircleSize / 2.0f, CircleSize),
+                &CircleBrush,
+                ESlateDrawEffect::None,
+                FLinearColor::Green // 색상
+            );
+        }
+    }
+
     return Super::NativePaint(Args, AllottedGeometry, MyCullingRect, OutDrawElements, CurrentLayer, InWidgetStyle, bParentEnabled);
 }
