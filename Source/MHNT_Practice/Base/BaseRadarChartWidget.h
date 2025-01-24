53353c9461ceda5018bba20ac7e20712564b8ce5
kykim9050 <kykim9050@naver.com>
2025-01-24 14:44:28 +0900

    김경윤 : IndustryDisasterChartWidget, BaseRadarChartWidget 클래스 추가

---
+++ Source/MHNT_Practice/Base/BaseRadarChartWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseRadarChartWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHNT_PRACTICE_API UBaseRadarChartWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MHNT_Practice/UI/Industry/IndustryDisasterChartWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Industry/Base/BaseIndustryWidget.h"
+#include "IndustryDisasterChartWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MHNT_PRACTICE_API UIndustryDisasterChartWidget : public UBaseIndustryWidget
+{
+	GENERATED_BODY()
+	
+};
